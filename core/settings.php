2825ff4e3be8ea71b6a12af75c3aa01301b4d66c
Panda <handsomebody1112@outlook.com>
2023-07-05 09:57:41 -0400

    update setting for db

--- core/settings.php
+++ core/settings.php
@@ -14,14 +14,14 @@
 $sql_db_host = "127.0.0.1";
 
 # MySQL db User
-$sql_db_user = "root";
+$sql_db_user = "meetahoe_pepe";
 
 # MySQL db Password
-$sql_db_pass = "";
+$sql_db_pass = "Mariana69!";
 
 # MySQL db Name
-$sql_db_name = "colibrism_db";
+$sql_db_name = "meetahoe_betapepe";
 
 # Site URL
-$site_url    = "http://localhost/ColibriSM"; // E.g (https://www.mysite.com)
+$site_url    = "https://beta.pepesocialnetwork.com"; // E.g (https://www.mysite.com)
 ?>
